9f8f006f1d4b5740e7aac90879eaa58ff5ab0968
limeiman <limeiman@weface.com>
2019-11-04 16:28:24 +0800

    first commit

---
+++ src/main/java/com/springbootgit/Cotroller/Cotroller.java
@@ -0,0 +1,19 @@
+package com.springbootgit.Cotroller;
+
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class Cotroller {
+
+
+    @RequestMapping("/hello")
+    public  String success(){
+
+        return "you are so beatuful";
+    }
+
+
+
+}
