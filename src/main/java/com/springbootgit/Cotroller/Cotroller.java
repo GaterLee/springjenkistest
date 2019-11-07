a4f11c00319eeed35d4462564f36b91a1e52ab25
limeiman <limeiman@weface.com>
2019-11-07 18:51:10 +0800

    每天都要开心

--- src/main/java/com/springbootgit/Cotroller/Cotroller.java
+++ src/main/java/com/springbootgit/Cotroller/Cotroller.java
@@ -11,7 +11,7 @@ public class Cotroller {
     @RequestMapping("/hello")
     public  String success(){
 
-        return "愿世间美好@与你环环相扣@此时已莺飞草长&&相爱的人在路上";
+        return "愿世间美好,与你环环相扣,此时已莺飞草长,相爱的人在路上!";
     }
 
 
